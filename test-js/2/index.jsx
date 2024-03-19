33b3a925b2fa7269ab1b122bb1bd2fe6929ce2d2
WinnieS0728 <116339731+WinnieS0728@users.noreply.github.com>
2024-03-19 16:57:19 +0800

    update promise

--- test-js/2/index.jsx
+++ test-js/2/index.jsx
@@ -1,6 +1,9 @@
+// import { getMonthSale } from "./promise";
+
 export default function JS_2() {
   return (
     <h3>
+      <p>請使用 getMonthSale 這個 function 來寫以下題目。</p>
       2-1. 算出今年所有銷售總和
       <br />
       2-2. 由銷售金額多到少列出 4-9 月各月份所有客戶的的銷售金額
--- test-js/2/promise.js
+++ test-js/2/promise.js
@@ -7,7 +7,25 @@ export async function getMonthSale({ YY, MM }) {
   return new Promise((resolve) => {
     (async function () {
       await sleep(3000);
-      resolve(dataSet.filter((data) => data.year === YY && data.month == MM));
+      switch (true) {
+        case !YY && !MM:
+          resolve(dataSet);
+          break;
+        case YY && !MM:
+          resolve(dataSet.filter((data) => data.YY === YY));
+          break;
+        case !YY && MM:
+          resolve(dataSet.filter((data) => data.MM === MM));
+          break;
+        case YY && MM:
+          resolve(
+            dataSet.filter((data) => data.year === YY && data.month == MM)
+          );
+          break;
+        default:
+          resolve(dataSet);
+          break;
+      }
     })();
   });
 }
