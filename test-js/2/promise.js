ccc2f1113a92ee09314787b8ac18b285a62e4dd7
WinnieS0728 <116339731+WinnieS0728@users.noreply.github.com>
2024-03-19 17:17:00 +0800

    update read.me

--- test-js/2/promise.js
+++ test-js/2/promise.js
@@ -6,18 +6,18 @@ async function sleep(time) {
 export async function getMonthSale({ YY, MM }) {
   return new Promise((resolve) => {
     (async function () {
-      await sleep(3000);
+      await sleep(1500);
       switch (true) {
         case !YY && !MM:
           resolve(dataSet);
           break;
-        case YY && !MM:
+        case !!YY && !MM:
           resolve(dataSet.filter((data) => data.YY === YY));
           break;
-        case !YY && MM:
+        case !YY && !!MM:
           resolve(dataSet.filter((data) => data.MM === MM));
           break;
-        case YY && MM:
+        case !!YY && !!MM:
           resolve(
             dataSet.filter((data) => data.year === YY && data.month == MM)
           );
